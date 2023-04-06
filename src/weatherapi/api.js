17e5c6fe9f1099227dfddb084a22ca3565e724b6
João da Cruz <102554786+JohnDaCruz@users.noreply.github.com>
2023-04-06 15:20:23 -0300

    Atualização da API logic

--- src/weatherapi/api.js
+++ src/weatherapi/api.js
@@ -1,21 +1,23 @@
-import { View, TextInput, TouchableOpacity, Text } from "react-native";
+import { View, TextInput, } from "react-native";
 import React, { useState, useEffect } from "react";
 import styles from "./apiStyle"
 import { API_KEY } from '@env'
 
 export default function Api() {
-    const [city, setCity] = useState('')
-    function enviar() {
-        setCity(city)
-        console.log('opa')
-    }
+
+    const [city, setCity] = useState('New york')
+
     useEffect(() => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data.name)
+                var temp = data.main.temp
+                var city = data.name
+                console.log(temp)
+                console.log(city)
+
             })
-    }, [])
+    }, [city])
     return (
         <View styles={styles.viewCitys}>
             <TextInput
@@ -24,10 +26,8 @@ export default function Api() {
                 inputMode="text"
                 placeholderTextColor={'#fff'}
                 value={city}
+                onChangeText={text => setCity(text)}
             />
-            <TouchableOpacity onPress={enviar}>
-                <Text>Enviar</Text>
-            </TouchableOpacity>
         </View>
     );
 }
